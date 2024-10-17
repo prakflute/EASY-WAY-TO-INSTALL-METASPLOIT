4d18a2a23dbf365a572c007b06ae9a21318ea328
prakflute <gondaliyaprakash927@gmail.com>
2024-10-17 17:52:23 +0530

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -1,17 +1,13 @@
-#!/usr/bin/env bash
+#bin!/user
 
-# Update package lists and upgrade installed packages
-pkg update && pkg upgrade -y
-
-# Install necessary packages
-pkg install curl gnupg -y
-
-# Download and install Metasploit
-curl -o msfinstall https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb && \
+apt update && yes | apt upgrade
+apt install curl
+apt-get install gnupg
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
 chmod 755 msfinstall && \
 bash msfinstall
 
-# Clean up
-pkg clean
 
 echo "All packages have been installed successfully!"
+
+    
